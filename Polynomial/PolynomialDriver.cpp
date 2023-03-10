8b9c012b457a6c2fb692b8ed11cedfc5b783f1c8
Shereen Khoja <shereen@pacificu.edu>
2023-03-10 10:20:25 -0800

    Add project files.

---
+++ Polynomial/PolynomialDriver.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Polynomial.h"
+
+using namespace std;
+
+int main() {
+
+  cout << "project terminated successfully";
+
+  return EXIT_SUCCESS;
+}
---
+++ Term/TermDriver.cpp
@@ -0,0 +1,8 @@
+#include "Term.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  return EXIT_SUCCESS;
+}
